50333fea048efe8069a6fb6b3d0c0a7698114af2
Jerry <1369161009@qq.com>
2026-07-15 01:07:51 +0800

    second summit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'index.html',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'app.html'),
+      },
     },
   },
 })
